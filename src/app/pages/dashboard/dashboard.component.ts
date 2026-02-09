79673169eee8a997d65d6ccf76246038aea833b6
PesanduJayasinghe <pesanduse@gmail.com>
2026-02-09 13:44:31 +0530

    Initial Commit

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [NavBarComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+
+}
