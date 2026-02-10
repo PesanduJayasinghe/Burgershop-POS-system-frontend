99ebed745b461fadcfb7680e9b8e71ad7384fb49
PesanduJayasinghe <pesanduse@gmail.com>
2026-02-10 10:18:59 +0530

    Added css globallty

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,20 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { BurgersComponent } from './components/card/burgers/burgers.component';
+import { BeveragesComponent } from './components/card/beverages/beverages.component';
+import { SweetsComponent } from './components/card/sweets/sweets.component';
+
 
 export const routes: Routes = [
-    {
-        path : "",
-        component : DashboardComponent
-    }
+  { 
+    path: '', 
+    component: DashboardComponent,
+    children: [
+      { path: '', redirectTo: 'burgers', pathMatch: 'full' },
+      { path: 'burgers', component: BurgersComponent },
+      { path: 'beverages', component: BeveragesComponent },
+      { path: 'sweets', component: SweetsComponent }
+    ]
+  }
 ];
+
