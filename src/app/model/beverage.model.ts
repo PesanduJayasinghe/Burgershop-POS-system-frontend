0488dd9bc33fd3b6c2ae2b46b9b5c376428db20e
PesanduJayasinghe <pesanduse@gmail.com>
2026-02-11 16:32:32 +0530

    Added interfaces to the model

---
+++ src/app/model/beverage.model.ts
@@ -0,0 +1,6 @@
+export interface Beverage{
+    // id : number;
+    title : string;
+    image : string;
+    price : string;
+}
---
+++ src/app/model/burger.model.ts
@@ -0,0 +1,6 @@
+export interface Burger{
+    // id : number;
+    title : string;
+    image : string;
+    price : string;
+}
