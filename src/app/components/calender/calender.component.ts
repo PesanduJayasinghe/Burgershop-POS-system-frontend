4a61e388ca332458cd8655e095952d4caf859831
PesanduJayasinghe <pesanduse@gmail.com>
2026-02-17 01:33:49 +0530

    Fixed calender not closing error

--- src/app/components/calender/calender.component.ts
+++ src/app/components/calender/calender.component.ts
@@ -64,6 +64,19 @@ export class CalendarComponent {
     }
   }
 
+  // Close calendar when clicking outside
+  @HostListener('document:click', ['$event'])
+  onClickOutside(event: MouseEvent): void {
+    if (this.inputWrapper && this.calendar) {
+      const clickedInside = this.inputWrapper.nativeElement.contains(event.target) || 
+                           this.calendar.nativeElement.contains(event.target);
+      
+      if (!clickedInside) {
+        this.isCalendarOpen = false;
+      }
+    }
+  }
+
   // Previous month
   previousMonth(): void {
     if (this.currentMonth === 0) {
